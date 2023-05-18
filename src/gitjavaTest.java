ccec6e2d1ce0d97ebb705d9f4732c0a3a1162413
1241028906 <zhiyuan.wang@atoz.com.cn>
2023-05-18 08:24:46 +0800

    Initial commit

---
+++ src/gitjavaTest.java
@@ -0,0 +1,13 @@
+/**
+ * @BelongsProject: gitTest
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: wangzhiyuan
+ * @CreateTime: 2023-05-18  08:21
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class gitjavaTest {
+    public static void main(String[] args) {
+
+    }
+}
